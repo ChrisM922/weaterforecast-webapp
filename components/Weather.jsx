f3b24640508e1757641f0f8a22fcd9ab0592dd63
ChrisM922 <112943995+ChrisM922@users.noreply.github.com>
2023-10-18 23:48:26 +0200

    Update .gitignore and add a README.md

---
+++ components/Weather.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from 'next/image'
+
+const watherData = ({ data }) => {
+
+  console.log(`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+
+  return (
+    <div className='relative flex flex-col justify-between max-w-[350px] w-full h-[90vh] m-auto p-4 text-gray-300 z-30'>
+      <div className='relative flex justify-between pt-12'>
+        <div className='flex flex-col items-center'>
+          <Image
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="/"
+            width="100"
+            height="100"
+          />
+          <p className='text-2xl'>{data.weather[0].main}</p>
+        </div>
+        <p className='text-9xl'>{((data.main.temp - 32) * 5 / 9).toFixed(0)}&#176;</p>
+      </div>
+      <div className='relative bg-black/75 p-8 rounded-md'>
+        <p className='text-2xl text-center pb-6'>Weather in {data.name}</p>
+        <div className='flex justify-between text-center'>
+          <div>
+            <p className='font-bold text-2xl'>{((data.main.feels_like - 32) * 5 / 9).toFixed(0)}&#176;</p>
+            <p className='text-xl'>Feels like</p>
+          </div>
+          <div>
+            <p className='font-bold text-2xl'>{data.main.humidity}%</p>
+            <p className='text-xl'>Humidity</p>
+          </div>
+          <div>
+            <p className='font-bold text-2xl'>{data.wind.speed.toFixed(0)} mph</p>
+            <p className='text-xl'>Wind</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default watherData
---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+import { BsSearch } from "react-icons/bs";
+import Image from "next/image";
+import { Input } from "postcss";
+import Weather from "@/components/Weather";
+import Spinner from "@/components/Spinner";
+export default function Home() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+    });
+    setCity("");
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        {/* Overlay */}
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/20 z-[1]' />
+
+        {/** Backgound Image */}
+        <Image
+          src='https://images.unsplash.com/photo-1516912481808-3406841bd33c?auto=format&fit=crop&q=80&w=3288&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='lightning'
+          layout='fill'
+          className='object-cover relative '
+        />
+
+        {/** Search */}
+        <div className='relative flex justify-between items-center max-w-[350px] w-full m-auto pt-4 text-white z-10'>
+          <form
+            onSubmit={fetchWeather}
+            className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'
+          >
+            <div>
+              <input
+                className='bg-transparent border-none text-white focus:outline-none text-2xl'
+                onChange={(e) => setCity(e.target.value)}
+                type='text'
+                placeholder='Search City'
+              />
+            </div>
+            <button onClick={fetchWeather}>
+              <BsSearch size={30} />
+            </button>
+          </form>
+        </div>
+
+        {/** Weather */}
+        {weather.main && <Weather data={weather} />}
+      </div>
+    );
+  }
+}
